180b77bee018d003f392b5af4088a1c9ca4e392d
iqbale123 <imuflihsin@gmail.com>
2022-10-02 20:43:39 +0700

    Hallo

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\Student as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Authenticatable
